2f75aceb599a6b76a0ebfc739500d55e56b21807
Adrian Parvin D. Ouano <adrianparvino@gmail.com>
2018-01-02 01:15:27 +0800

    Added terminal size stuff.

---
+++ terminal.c
@@ -0,0 +1,40 @@
+/* This file is part of ASCIIVN.
+ *
+ * Copyright (C) 2017  Adrian Parvin D. Ouano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/ioctl.h>
+
+#include "terminal.h"
+
+int
+get_terminal_width()
+{
+	struct winsize w;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+	
+	return w.ws_col;
+}
+
+int
+get_terminal_height()
+{
+	struct winsize w;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
+	return w.ws_row;
+}
